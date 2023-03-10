e45fa96851f2a6cd0ddf167938ad30a69f9b9b29
kelamJ <malekjulien80@gmail.com>
2023-03-10 12:23:55 +0100

    update

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,17 +2,29 @@
 
 namespace App\DataFixtures;
 
+use Generator;
+use Faker\Factory;
 use App\Entity\Tierlist;
-use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
+    /*
+    * @var Generator
+    */
+    private Generator $faker;
+
+    public function __construct()
+    {
+        $this->faker = Factory::create('fr_FR');
+    }
+
     public function load(ObjectManager $manager): void
     {
-        for ($i = 1; $i <= 50; $i++) { 
+        for ($i = 0; $i < 50; $i++) { 
             $tierlist = new Tierlist();
-            $tierlist->setName('Tierlist ' . $i)
+            $tierlist->setName($this->faker->word())
                 ->setType('Gacha');
                 
                 $manager->persist($tierlist);
