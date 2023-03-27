87784cd0fb2d5eab13a5a4e1ebe8c71eebd3d40d
kelamJ <malekjulien80@gmail.com>
2023-03-27 16:24:18 +0200

    Setting up login form

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,16 +9,12 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    #[Route('/connexion', name: 'security-login')]
+    #[Route('/connexion', 'security-login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        $error = $authenticationUtils->getLastAuthenticationError();
-
-        $lastUsername = $authenticationUtils->getLastUsername();
-
         return $this->render('pages/security/login.html.twig', [
-            'last_username' => $lastUsername,
-            'error'         => $error,
+            'last_username' => $authenticationUtils->getLastUsername(),
+            'error'         => $authenticationUtils->getLastAuthenticationError(),
         ]);
     }
 
